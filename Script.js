a946d996284d8f551520d4efc1dff83afdc0c1fb
LukasDrews1 <84077918+LukasDrews1@users.noreply.github.com>
2022-09-28 15:49:17 +0200

    Add a Array

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
 const Answer = document.getElementById("Answer");
 const Input = document.getElementById("Input-style");
 const Enter = document.getElementById("Enter");
+let Answers = ["Yes", "No", "it's not likely", "It seems to be"];
