4e4b6fb1fe9c9c67b0e329299b356da733e0461b
LukasDrews1 <84077918+LukasDrews1@users.noreply.github.com>
2022-09-29 08:59:39 +0200

    Add a Enter.addEventListener

--- Script.js
+++ Script.js
@@ -2,3 +2,8 @@ const Answer = document.getElementById("Answer");
 const Input = document.getElementById("Input-style");
 const Enter = document.getElementById("Enter");
 let Answers = ["Yes", "No", "it's not likely", "It seems to be"];
+
+Enter.addEventListener('click', function(){
+	Input.innerHTML = "";
+    Answer.innerHTML = Answers[0];
+});
