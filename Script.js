5c752853df387a3ed5b4db7afeafb398b49e72cb
LukasDrews1 <84077918+LukasDrews1@users.noreply.github.com>
2022-09-29 09:08:41 +0200

    Add a RandomAnswers

--- Script.js
+++ Script.js
@@ -1,7 +1,9 @@
 const Answer = document.getElementById("Answer");
 const Input = document.getElementById("Input-style");
 const Enter = document.getElementById("Enter");
+
 let Answers = ["Yes", "No", "it's not likely", "It seems to be"];
+let RandomAnswers = Math.floor(Math.random() * 5) + 1;
 
 Enter.addEventListener('click', function(){
 	Input.innerHTML = "";
