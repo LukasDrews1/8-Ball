aac87f5286d1042f5e7457f35a7c7cbc82503b50
LukasDrews1 <84077918+LukasDrews1@users.noreply.github.com>
2022-09-29 10:10:00 +0200

    change RandomAnswers

--- Script.js
+++ Script.js
@@ -3,9 +3,10 @@ const Input = document.getElementById("Input-style");
 const Enter = document.getElementById("Enter");
 
 let Answers = ["Yes", "No", "it's not likely", "It seems to be"];
-let RandomAnswers = Math.floor(Math.random() * 5) + 1;
+let RandomAnswers = Math.floor(Math.random() * 4) + 1;
 
 Enter.addEventListener('click', function(){
 	Input.value = "";
     Answer.innerHTML = Answers[RandomAnswers];
+    console.log(RandomAnswers);
 });
